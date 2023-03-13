e31ee7d3566fb3e1cd2ead800a000ece7352cdac
Boy <easonwu.student@outlook.com>
2023-03-13 11:52:37 +0800

    first commit

---
+++ src/Page/project.js
@@ -0,0 +1,57 @@
+const callouts = [
+    {
+      name: '樹梅派溫溼度感測',
+      description: '類型：團體開發',
+      imageSrc: "https://i.imgur.com/4stXFR9.png",
+      imageAlt: '圖片載入失敗',
+      href: 'https://github.com/PicassoEason/PI-GetTH.git',
+    },
+    {
+      name: '火場VR逃生',
+      description:  '類型：團體開發',
+      imageSrc: 'https://i.imgur.com/wViMUNj.png',
+      imageAlt: '圖片載入失敗',
+      href: '#',
+    },
+    {
+      name: 'Travel',
+      description: 'Daily commute essentials',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
+      imageAlt: '圖片載入失敗',
+      href: '#',
+    },
+  ]
+  
+  export default function Example() {
+    return (
+      <div className="bg-gray-100">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
+            <h2 className="text-2xl font-bold text-gray-900">作品集</h2>
+  
+            <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
+              {callouts.map((callout) => (
+                <div key={callout.name} className="group relative">
+                  <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
+                    <img
+                      src={callout.imageSrc}
+                      alt={callout.imageAlt}
+                      className="h-full w-full object-cover object-center"
+                    />
+                  </div>
+                  <h3 className="mt-6 text-sm text-gray-500">
+                    <a href={callout.href}>
+                      <span className="absolute inset-0" />
+                      {callout.name}
+                    </a>
+                  </h3>
+                  <p className="text-base font-semibold text-gray-900">{callout.description}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
