6663ef42cda5317838d943d5ae5f4b0af5bff5c6
Boy <easonwu.student@outlook.com>
2023-03-13 17:25:20 +0800

    delete button
    ;

--- src/Page/nav.js
+++ src/Page/nav.js
@@ -61,15 +61,7 @@ export default function Nav() {
                   </div>
                 </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-
-              </div>
+        
             </div>
           </div>
 
